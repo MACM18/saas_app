c43427480864271a7078b8e10f062df84a80f9c0
macm18 <machathuramadushanka@outlook.com>
2024-02-19 18:41:39 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/shared/Sidebar";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p> Home</p>
+    </div>
+  );
+};
+
+export default Home;
